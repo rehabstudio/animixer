f0a6744ddb0fd7240bcf93a5b0ed7f4e327d5938
Noel Wilson <noel.wilson@rehabstudio.com>
2018-01-10 16:23:31 +0000

    Created Chatbox component

--- webapp/src/components/App/ChatBox.js
+++ webapp/src/components/App/ChatBox.js
@@ -7,43 +7,110 @@
 /* @flow */
 
 import React from 'react';
+import styled from 'styled-components';
+import {ApiAiClient} from "api-ai-javascript";
+
+const ENTER_KEY_CODE = 13;
+
+const Container = styled.div`
+  height: calc(100vh - 250px);
+  height: -o-calc(100vh - 250px); /* opera */
+  height: -webkit-calc(100vh - 250px); /* google, safari */
+  height: -moz-calc(100vh - 250px); /* firefox */
+  overflow-y: auto;
+`;
 
 class ChatBox extends React.Component<{}, {}> {
 
   constructor(props) {
     super(props);
     this.state = {
-      client = new ApiAi.ApiAiClient({accessToken: this.props.accessToken});
+      client: new ApiAiClient({accessToken: this.props.accessToken})
     };
+  }
+
+  componentDidMount() {
+    this.inputField.addEventListener("keydown", this.queryInputKeyDown.bind(this));
+  }
+
+  componentWillUnmount() {
+    this.inputField.removeEventListener("keydown", this.queryInputKeyDown.bind(this));
+  }
+
+  queryInputKeyDown(event) {
+    if (event.which !== ENTER_KEY_CODE) {
+      return;
+    }
+
+    let value = this.inputField.value;
+    this.inputField.value = "";
+
+    this.createQueryNode(value);
+    let responseNode = this.createResponseNode();
+
+    this.sendText(value)
+      .then(function(response) {
+        let result;
+        try {
+          result = response.result.fulfillment.speech
+        } catch(error) {
+          result = "";
+        }
+        this.setResponseOnNode(result, responseNode);
+      }.bind(this))
+      .catch(function(err) {
+        console.log(err);
+        this.setResponseOnNode("Something goes wrong", responseNode);
+      }.bind(this));
+  }
+
+  sendText(text) {
+    return this.state.client.textRequest(text);
+  }
+
+  createQueryNode(query) {
+    let node = document.createElement('div');
+    node.className = "clearfix left-align left card-panel green accent-1";
+    node.innerHTML = query;
+    this.resultDiv.appendChild(node);
+  }
+
+  createResponseNode() {
+    let node = document.createElement('div');
+    node.className = "clearfix right-align right card-panel blue-text text-darken-2 hoverable";
+    node.innerHTML = "...";
+    this.resultDiv.appendChild(node);
+    this.updateScroll();
+    return node;
+  }
+
+  setResponseOnNode(response, node) {
+    node.innerHTML = response ? response : "[empty response]";
+    node.setAttribute('data-actual-response', response);
+  }
+
+  updateScroll(){
+    this.chatDiv.scrollTop = this.chatDiv.scrollHeight;
   }
 
   render() {
     return (
-      <div class="container">
-          <div class="row">
-              <div class="input-field col s10">
-                  <input id="access_token" type="text" />
-                  <label for="access_token">Access token</label>
-              </div>
-              <div class="col s2 input-field">
-                  <a class="waves-effect waves-light btn" id="set_access_token">Set</a>
-              </div>
-          </div>
+      <Container innerRef={(ele) => this.chatDiv = ele} className="container">
           <div id="placeholder">
-              <h5>Please, fill access token before start</h5>
+              <h5>Say "hello" to talk to animxier</h5>
           </div>
           <div id="main-wrapper">
-              <div class="row">
-                  <div class="col s10">
-                      <div id="result">
+              <div className="row">
+                  <div className="col s12">
+                      <div ref={(ele) => this.resultDiv = ele} id="result">
                       </div>
-                      <div class="input-field">
-                          <input placeholder="Hey, ask me something..." id="q" type="text" />
+                      <div className="input-field">
+                          <input ref={(ele) => this.inputField = ele} placeholder="Hey, ask me something..." id="q" type="text" />
                       </div>
                   </div>
               </div>
           </div>
-      </div>
+      </Container>
     );
   }
 }
