a7e99a2ff7c0d366278731a844cb9d281fa7378a
Noel Wilson <noel.wilson@rehabstudio.com>
2018-09-11 16:36:39 +0100

    require typo

--- functions/config.js
+++ functions/config.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-const serviceAccount = require('certs/service_account.json');
+const serviceAccount = require('./certs/service_account.json');
 
 process.env.DEBUG = 'actions-on-google:*';
 
